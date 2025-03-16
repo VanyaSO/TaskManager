46bb687639036593189013460aa3b903c572ae05
Ivan Ushachov <ushachovg324@gmail.com>
2025-03-16 20:06:35 +0200

    created Dashboard component

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,10 +1,55 @@
+import {useState} from "react";
+import {Button, Divider, Flex} from "antd";
+import {MainLayout} from "../../widgets/MainLayout/MainLayout.tsx";
+import {SearchBar} from "../../components/SearchBar/SearchBar.tsx";
+import {TasksSection} from "../../components/TasksSection/TasksSection.tsx";
+import {ReloadOutlined} from "@ant-design/icons";
+import {Task} from "../../types/Task.ts";
+import {selectTasks} from "../../store/selectors.ts";
+import {useSelector} from "react-redux";
 import styles from "./Dashboard.module.scss";
-import { MainLayout } from "../../widgets/MainLayout/MainLayout.tsx";
 
 export function Dashboard() {
-  return (
-    <MainLayout>
-      <div className={styles.root}>Dashboard</div>
-    </MainLayout>
-  );
+    const [activeSection, setActiveSection] = useState<string>("tasks");
+    const tasks: Task[] = useSelector(selectTasks);
+
+    const handleClickTasksTab = () => {
+        setActiveSection("tasks");
+    };
+
+    const handleClickArchiveTab = () => {
+        setActiveSection("archive");
+    };
+
+    const refetch = () => {
+    };
+
+    return (
+        <MainLayout>
+            <div className={styles.root}>
+                <div className="container">
+                    <Flex justify="space-between">
+                        <Flex gap={10}>
+                            <Button
+                                type={activeSection === "tasks" ? "primary" : "default"}
+                                onClick={handleClickTasksTab}
+                            >
+                                Tasks
+                            </Button>
+                            <Button
+                                type={activeSection === "archive" ? "primary" : "default"}
+                                onClick={handleClickArchiveTab}
+                            >
+                                Archive
+                            </Button>
+                            <Button type="text" onClick={refetch} icon={<ReloadOutlined/>}/>
+                        </Flex>
+                        <SearchBar/>
+                    </Flex>
+                    <Divider/>
+                    <TasksSection tasks={tasks}/>
+                </div>
+            </div>
+        </MainLayout>
+    );
 }
